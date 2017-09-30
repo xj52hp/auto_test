04216ee9f288f24e63adf599a7e5b1487ca80b91
xj52hp <xj52hp@gmail.com>
2017-09-30 13:01:33 +0800

    create

--- monkeyTest-double/monkeyTest.py
+++ monkeyTest-double/monkeyTest.py
@@ -9,7 +9,7 @@ from multiprocessing import Process
 
 from Base.BasePickle import writeInfo, writeSum, readInfo
 from Base.BaseWriteReport import report
-__author__ = 'shikun'
+
 import datetime
 import uuid
 import time
@@ -31,7 +31,8 @@ ba = AdbCommon.AndroidDebugBridge()
 
 
 info = []
-
+info_dir = "info/"
+log_dir = "log"
 
 # 手机信息
 def get_phome(devices):
@@ -46,25 +47,24 @@ def get_phome(devices):
 
 def mkdirInit(devices, app, data=None):
     # destroy(devices)
-    cpu = PATH("./info/" + devices + "_cpu.pickle")
-    men = PATH("./info/" + devices + "_men.pickle")
-    flow = PATH("./info/" + devices + "_flow.pickle")
-    battery = PATH("./info/" + devices + "_battery.pickle")
-    fps = PATH("./info/" + devices + "_fps.pickle")
+    cpu = PATH(info_dir + devices + "_cpu.pickle")
+    men = PATH(info_dir + devices + "_men.pickle")
+    flow = PATH(info_dir + devices + "_flow.pickle")
+    battery = PATH(info_dir + devices + "_battery.pickle")
+    fps = PATH(info_dir + devices + "_fps.pickle")
     app[devices] = {"cpu": cpu, "men": men, "flow": flow, "battery": battery, "fps": fps, "header": get_phome(devices)}
     OperateFile(cpu).mkdir_file()
     OperateFile(men).mkdir_file()
     OperateFile(flow).mkdir_file()
     OperateFile(battery).mkdir_file()
     OperateFile(fps).mkdir_file()
-    OperateFile(PATH("./info/sumInfo.pickle")).mkdir_file() # 用于记录是否已经测试完毕，里面存的是一个整数
-    OperateFile(PATH("./info/info.pickle")).mkdir_file() # 用于记录统计结果的信息，是[{}]的形式
+    OperateFile(PATH(info_dir + "sumInfo.pickle")).mkdir_file() # 用于记录是否已经测试完毕，里面存的是一个整数
+    OperateFile(PATH(info_dir + "info.pickle")).mkdir_file() # 用于记录统计结果的信息，是[{}]的形式
 
-    writeSum(0, data, PATH("./info/sumInfo.pickle")) # 初始化记录当前真实连接的设备数
+    writeSum(0, data, PATH(info_dir + "sumInfo.pickle")) # 初始化记录当前真实连接的设备数
 
 def runnerPool():
-    shutil.rmtree((PATH("./info/")))  # 删除持久化目录
-    os.makedirs(PATH("./info/")) # 创建持久化目录
+
     devices_Pool = []
     devices = ba.attached_devices()
     if devices:
@@ -85,12 +85,15 @@ def start(devicess):
     devices = devicess["devices"]
     num = devicess["num"]
     app = {}
+    print("devices---->", devices)
+    print("num---->", num)
+    print("app---->", app)
     mkdirInit(devices, app, num)
     mc = BaseMonkeyConfig.monkeyConfig(PATH("monkey.ini"))
     # 打开想要的activity
     # ba.open_app(mc["package_name"], mc["activity"], devices) 留着备用可以统计每次打开哪个页面的启动时间等
     # monkey开始测试
-    mc["log"] = PATH("./log") + "\\" + str(uuid.uuid4())
+    mc["log"] = PATH(log_dir) + "\\" + str(uuid.uuid4())
     mc["monkey_log"] = mc["log"] + "monkey.log"
     mc["cmd"] = mc['cmd'] + mc["monkey_log"]
     start_monkey("adb -s " + devices + " shell " + mc["cmd"], mc["log"])
@@ -110,21 +113,21 @@ def start(devicess):
             if monkeylog.read().count('Monkey finished') > 0:
                 endtime = datetime.datetime.now()
                 print(str(devices)+"测试完成咯")
-                writeSum(1, path=PATH("./info/sumInfo.pickle"))
+                writeSum(1, path=PATH(info_dir + "sumInfo.pickle"))
                 app[devices] ["header"]["beforeBattery"] = beforeBattery
                 app[devices]["header"]["afterBattery"] = BaseMonitor.get_battery(devices)
                 app[devices]["header"]["net"] = mc["net"]
                 app[devices]["header"]["monkey_log"] = mc["monkey_log"]
                 app[devices]["header"]["time"] = str((endtime - starttime).seconds) + "秒"
-                writeInfo(app, PATH("./info/info.pickle"))
+                writeInfo(app, PATH(info_dir + "info.pickle"))
                 break
                     # go.info[devices]["header"]["sumTime"] = str((endtime - starttime).seconds) + "秒"
                     # report(go.info)
-    if readInfo(PATH("./info/sumInfo.pickle")) <= 0:
-        print(readInfo(PATH("./info/info.pickle")))
-        report(readInfo(PATH("./info/info.pickle")))
+    if readInfo(PATH(info_dir + "sumInfo.pickle")) <= 0:
+        print(readInfo(PATH(info_dir + "info.pickle")))
+        report(readInfo(PATH(info_dir + "info.pickle")))
         subprocess.Popen("taskkill /f /t /im adb.exe", shell=True)
-        # shutil.rmtree((PATH("./info/"))) # 删除持久化目录
+        # shutil.rmtree((PATH(info_dir))) # 删除持久化目录
         print("------来吧------")
 
 
@@ -148,9 +151,19 @@ def killport():
     os.system(PATH('./kill5037.bat'))
     os.popen("adb kill-server adb")
     os.popen("adb start-server")
+
+def init_dir():
+    if os.path.exists(info_dir):
+        shutil.rmtree((PATH(info_dir)))  # 删除持久化目录
+    os.makedirs(PATH(info_dir))  # 创建持久化目录
+    if os.path.exists(log_dir):
+        shutil.rmtree((PATH(log_dir)))  # 删除持久化目录
+    os.makedirs(PATH(log_dir))  # 创建持久化目录
+
 if __name__ == '__main__':
     killport()
     time.sleep(1)
+    # init_dir()
     runnerPool()
     # p = Process(target=runnerPool, args=())
     # p.start()
